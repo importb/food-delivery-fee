369fff2751276532e96bdeac9b8d2d1c9a54f403
importb <rainervana1@gmail.com>
2026-03-20 12:23:55 +0200

    chore: initialize project with spring boot starter

---
+++ src/main/java/com/delivery/fee/FoodDeliveryFeeApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.fee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryFeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryFeeApplication.class, args);
+    }
+
+}
